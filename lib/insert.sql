15881779a7bc6af0ef04753dcc16adb16a9da7d0
Austin  Burke <aburk3@gmail.com>
2018-11-14 12:04:26 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -17,7 +17,7 @@ INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES
   (5, "Character One", "motto one", "earth", 2, 2), (6, "Character Two", "motto two", "fire", 2, 2), (7, "Character Three", "motto three", "wind", 2, 2), (8, "Character Four", "motto four", "water", 2, 2);
 
 INSERT INTO character_books (id, book_id, character_id) VALUES
-  (1, 1, 1), (2, 2, 1), (3, 3, 1), (4, 4, 1), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 4, 3), (9, 1, 2), (10, 3, 4);
+  (1, 1, 1), (2, 2, 1), (3, 3, 1), (4, 1, 3), (5, 2, 3), (6, 3, 3), (7, 2, 2), (8, 4, 4);
 
 INSERT INTO character_books (id, book_id, character_id) VALUES
-  (1, 1, 1), (2, 2, 1), (3, 3, 1), (4, 4, 1), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 4, 3), (9, 1, 2), (10, 3, 4);
+  (9, 4, 5), (10, 5, 5), (11, 6, 5), (12, 4, 6), (13, 5, 6), (14, 6, 6), (15, 5, 7), (16, 7, 8);
